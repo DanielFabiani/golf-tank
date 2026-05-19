3352a09243d002583b45d7c37c30dc72108b17f5
Daniel Fabiani <danielfabiani.dev@gmail.com>
2026-05-19 13:15:09 -0300

    feat: add Hero component with branding and sponsor logos to landing page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
       />
       <div className="absolute inset-0 bg-black/40 bg-[linear-gradient(to_top,rgba(0,21,3,0.8)_0%,rgba(0,21,3,0.4)_50%,rgba(0,21,3,0.2)_100%)]" />
 
-      <div className="relative z-[2] flex flex-col items-center text-center px-5 my-10 sm:mt-0">
+      <div className="relative z-[2] flex flex-col items-center text-center px-5 my-10 sm:mt-10">
         {/* Badge edición */}
 
         <p
@@ -34,7 +34,7 @@ export default function Hero() {
 
         {/* Logo Golf Tank */}
         <div
-          className="w-[220px] sm:w-[300px] md:w-[380px] animate-fadeUp flex flex-col items-center justify-center gap-3"
+          className="w-[80%] sm:w-[70%] md:w-[55%] max-w-[550px] animate-fadeUp flex flex-col items-center justify-center gap-3"
           style={{ animationDelay: "0.25s" }}
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -60,20 +60,20 @@ export default function Hero() {
 
         {/* Logos sponsors */}
         <div
-          className="flex items-center gap-6 animate-fadeUp"
+          className="flex items-center justify-center gap-6 sm:gap-10 animate-fadeUp w-[85%] sm:w-[70%] md:w-[55%] max-w-[550px]"
           style={{ animationDelay: "0.55s" }}
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src="/lakaut.svg"
             alt="Lakaut"
-            className="h-20 sm:h-28 w-auto brightness-0 invert drop-shadow-md"
+            className="w-[55%] h-auto brightness-0 invert drop-shadow-md"
           />
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src="/FID.svg"
             alt="FID by Lakaut"
-            className="h-24 sm:h-36 w-auto brightness-0 invert drop-shadow-md"
+            className="w-[45%] h-auto brightness-0 invert drop-shadow-md"
           />
         </div>
       </div>
